3529ad8fb4ff7131fd7cb3f640f47ab6d341f1b4
shuai <1627392348@qq.com>
2018-11-30 18:07:59 +0800

    web,异常

--- src/main/java/com/shuai/controller/EmployeeController.java
+++ src/main/java/com/shuai/controller/EmployeeController.java
@@ -4,12 +4,14 @@ import com.shuai.dao.DepartmentDao;
 import com.shuai.dao.EmployeeDao;
 import com.shuai.entities.Department;
 import com.shuai.entities.Employee;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import javax.websocket.server.PathParam;
 import java.util.Collection;
 
 /**
@@ -21,6 +23,8 @@ import java.util.Collection;
 @Controller
 public class EmployeeController {
 
+	private Logger logger = LoggerFactory.getLogger(getClass());
+
 	@Autowired
 	EmployeeDao employeeDao;
 
@@ -29,38 +33,85 @@ public class EmployeeController {
 
 	/**
 	 * 查询所有员工返回列表页面
+	 *
 	 * @param modelMap
 	 * @return
 	 */
 	@GetMapping("/emps")
-	public String list(ModelMap modelMap){
+	public String list(ModelMap modelMap) {
 		Collection<Employee> all = employeeDao.getAll();
-		modelMap.put("emps",all);
+		modelMap.put("emps", all);
 		return "emp/list";
 	}
 
 	/**
 	 * 跳转到添加页面
+	 *
 	 * @return
 	 */
 	@GetMapping("/emp")
-	public String toAddPage(ModelMap modelMap){
+	public String toAddPage(ModelMap modelMap) {
 		//查询所有部门
 		Collection<Department> departments = departmentDao.getDepartments();
-		modelMap.put("depts",departments);
+		modelMap.put("depts", departments);
 		return "emp/add";
 	}
 
 	/**
 	 * 员工添加
+	 *
 	 * @return
 	 */
 	@PostMapping("/emp")
-	public String addEmp(Employee employee){
-
+	public String addEmp(Employee employee) {
+		logger.info("添加员工");
+		logger.debug("员工信息：{}",employee);
+		employeeDao.save(employee);
 		//来到员工列表页面
 		//redirect：表示重定向到一个地址
 		//forward：表示转发到一个地址
+		return "redirect:/emps";
+	}
+
+	/**
+	 * 查出当前员工，跳转到修改页面，回显
+	 * @return
+	 */
+	@GetMapping("/emp/{id}")
+	public String toEditPage(ModelMap modelMap,@PathVariable("id") Integer id){
+		logger.info("修改员工跳转");
+		Employee employee = employeeDao.get(id);
+		logger.debug("当前员工：{}",employee);
+		//查询所有部门
+		Collection<Department> departments = departmentDao.getDepartments();
+		modelMap.put("depts", departments);
+		modelMap.put("emp",employee);
+		//回到修改页面（add是一个修改添加二合一的页面）
+		return "emp/add";
+	}
+
+	/**
+	 * 员工修改
+	 * @param employee
+	 * @return
+	 */
+	@PutMapping("/emp")
+	public String updateEmployee(Employee employee){
+		logger.info("员工修改");
+		logger.debug("修改后员工信息：{}",employee);
+		employeeDao.save(employee);
+		return "redirect:/emps";
+	}
+
+	/**
+	 * 员工删除
+	 * @return
+	 */
+	@DeleteMapping("/emp/{id}")
+	public String deleteEmployee(@PathVariable("id") Integer id){
+		logger.info("员工删除");
+		logger.debug("员工id={}",id);
+		employeeDao.delete(id);
 		return "redirect:/emps";
 	}
 }
--- src/main/java/com/shuai/controller/LoginController.java
+++ src/main/java/com/shuai/controller/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 /**
@@ -26,16 +25,16 @@ public class LoginController {
 //	@RequestMapping(value="/user/login",method = RequestMethod.POST)
 	public String login(ModelMap modelMap, HttpSession session, @RequestParam("username") String username, @RequestParam("password") String password) {
 		logger.info("开始登录验证");
-		logger.debug("登录名：{}",username);
-		logger.debug("登录密码：{}",password);
-		if(!StringUtils.isEmpty(username) && "123456".equals(password)){
+		logger.debug("登录名：{}", username);
+		logger.debug("登录密码：{}", password);
+		if (!StringUtils.isEmpty(username) && "123456".equals(password)) {
 			logger.info("登录成功");
-			session.setAttribute("loginUser",username);
+			session.setAttribute("loginUser", username);
 			//登录成功，防止表单重复提交，可以重定向到主页
 			return "redirect:/main.html";
 		} else {
 			logger.error("用户名密码错误");
-			modelMap.put("msg","用户名密码错误");
+			modelMap.put("msg", "用户名密码错误");
 			return "login";
 		}
 	}
