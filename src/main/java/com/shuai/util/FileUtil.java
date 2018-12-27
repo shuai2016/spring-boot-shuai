20a425c5353840fb2d2e8c3e55d0f3094f37fc1b
shuai <1627392348@qq.com>
2018-12-27 20:52:38 +0800

    water

--- src/main/java/com/shuai/util/FileUtil.java
+++ src/main/java/com/shuai/util/FileUtil.java
@@ -1,36 +1,61 @@
 package com.shuai.util;
- 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
- 
+
+import com.alibaba.fastjson.JSON;
+
+import java.io.*;
+
+/**
+ * FileUtil
+ *
+ * @author shuai
+ * @date 2018/12/27
+ */
 public class FileUtil {
- 
-    public static String ReadFile(String Path){
-        BufferedReader reader = null;
-        String laststr = "";
-        try{
-            FileInputStream fileInputStream = new FileInputStream(Path);
-            InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
-            reader = new BufferedReader(inputStreamReader);
-            String tempString = null;
-            while((tempString = reader.readLine()) != null){
-                laststr += tempString;
-            }
-            reader.close();
-        }catch(IOException e){
-            e.printStackTrace();
-        }finally{
-            if(reader != null){
-                try {
-                    reader.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        return laststr;
-    }
- 
+
+	public static String readFile(String Path) {
+		BufferedReader reader = null;
+		String lastStr = "";
+		try {
+			FileInputStream fileInputStream = new FileInputStream(Path);
+			InputStreamReader inputStreamReader = new InputStreamReader(fileInputStream, "UTF-8");
+			reader = new BufferedReader(inputStreamReader);
+			String tempString = null;
+			while ((tempString = reader.readLine()) != null) {
+				lastStr += tempString;
+			}
+			reader.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			if (reader != null) {
+				try {
+					reader.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return lastStr;
+	}
+
+	public static void writeFile(Object o, String Path) {
+		String strData = JSON.toJSONString(o);
+		System.out.println(strData);
+		File file = new File(Path);
+		Writer writer = null;
+		try {
+			file.createNewFile();
+			writer = new FileWriter(file);
+			writer.write(strData);
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
 }
